027084f4b33287258dbc5ce549996554011c0606
Danila Senin <16502711@mail.ru>
2023-10-15 23:11:57 +0300

    Number 3

---
+++ Number3/Program.cs
@@ -0,0 +1,18 @@
+﻿string Ned(int a)
+{
+    if (a == 6 || a == 7 ) 
+    { 
+        return "Да"; 
+    }
+    else if (a < 6 && a > 0) 
+    { 
+        return "Нет";
+    }
+    else 
+    { 
+        return "Нет такого дня недели"; 
+    }}
+Console.Write("Введите номер дня недели: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine(Ned(a));
+
