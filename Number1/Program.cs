1dfa4122e8a2bebd936bb37caa4134fcbde8c5c6
Danila Senin <16502711@mail.ru>
2023-10-15 23:18:52 +0300

    Final version

--- Number1/Program.cs
+++ Number1/Program.cs
@@ -1,3 +1,14 @@
-﻿Console.WriteLine("Введите трёхзначное число: ");
+﻿/*Console.WriteLine("Введите трёхзначное число: ");
 string a = Console.ReadLine();
 Console.Write(a[1]);
+*/
+
+int Ten(int a) 
+{
+    int res = a / 10 % 10;
+    return res; 
+}
+
+Console.WriteLine("Введите трёхзначное число: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Ваша вторая цифра: " + Ten(a));
